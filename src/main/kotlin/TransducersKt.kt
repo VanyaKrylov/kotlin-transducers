966bdd949a66298c9f3b42ca6402a83f80d178d1
Ivan Krylov <danieldvorzhetskii@gmail.com>
2020-11-26 19:52:37 +0300

    Transducer-based map functor extension for Lists

--- src/main/kotlin/TransducersKt.kt
+++ src/main/kotlin/TransducersKt.kt
@@ -1,6 +1,6 @@
 typealias Reducer<T,R> = (T, R) -> T
 
-typealias Transducer<T_n,T_m,R> = (Reducer<T_n, R>) -> Reducer<T_m, R>
+typealias Transducer<Acc,T_in,T_out> = (Reducer<Acc,T_in>) -> Reducer<Acc,T_out>
 
 fun <R,T> reduce(arr: Collection<T>, f: (R?,T) -> R): R {
     var acc: R? = null
@@ -22,16 +22,28 @@ fun <R,T> foldl(arr: Collection<T>, start: R, f: (R, T) -> R): R {
 fun <R,T> map(arr: Collection<T>, f: (T) -> R,): Collection<R> =
     foldl(arr, emptyList(),) { acc, el -> acc.plus(f(el)) }
 
-fun <R,A,T> mapping(f: (R) -> T) =
-    { step: Reducer<A,T> ->
-        { acc: A, arg: R -> step(acc, f(arg)) }}
+inline fun <Acc,T_in,T_out> mapping(crossinline f: (T_out) -> T_in) =
+    { step: Reducer<Acc,T_in> ->
+        { acc: Acc, arg: T_out -> step(acc, f(arg)) }}
 
+inline fun <T_in,T_out> List<T_out>.mapT(crossinline f: (T_out) -> T_in): List<T_in> {
+    val result = mutableListOf<T_in>()
+    for (el in this) {
+        (mapping<MutableList<T_in>,T_in,T_out>(f))(::conj).invoke(result, el)
+    }
+    return result
+}
+
+fun <T> conj(acc: MutableList<T>, el: T): MutableList<T> =
+    acc.apply { this.add(el) }
 
 fun Int.showDoubledString() = (this * this).toString()
 
 fun main() {
     val list = listOf(1,2,3)
     println("Casual: ${map(list) { it.showDoubledString() }}")
-    val foo = mapping<Int,List<String>,String>(Int::showDoubledString).invoke(List<String>::plus)
+    val foo = mapping<List<String>,String,Int>(Int::showDoubledString)
+        .invoke(List<String>::plus)
     println("Transduced: ${foldl(list, emptyList(), foo)}")
+    println("Ext fun: ${list.mapT { it.showDoubledString() }}")
 }
