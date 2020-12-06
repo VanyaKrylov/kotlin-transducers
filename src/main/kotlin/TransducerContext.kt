aeba8621480b4a774a7cc8db8d09427f7a6f3e85
Ivan Krylov <danieldvorzhetskii@gmail.com>
2020-12-06 21:10:47 +0300

    FlatMapping and taking functors done. PoC of exit var for flatMapiing

--- src/main/kotlin/TransducerContext.kt
+++ src/main/kotlin/TransducerContext.kt
@@ -1,9 +1,11 @@
-class TransducerContext<Recv>(var step: Reducer<Recv,String>) { //initially step is the terminating reduce function of the chain
+class TransducerContext<Recv,In,Out>(var step: Reducer<Recv,Out>) { //initially step is the terminating reduce function of the chain
+
+    var exit: Boolean = false
 
     inline fun ctx(
-        transduce: TransducerContext<Recv>.(Reducer<Recv,String>) -> Reducer<Recv,Int>
-    ): Reducer<Recv, Int> =
-        this.transduce(step)
+        apply: TransducerContext<Recv,In,Out>.(Reducer<Recv,Out>) -> Reducer<Recv,In>
+    ): Reducer<Recv, In> =
+        this.apply(step)
 
     inline fun <T_in,T_out> mapping(crossinline f: (T_out) -> T_in): Transducer<Recv,T_in,T_out> =
         { step: Reducer<Recv,T_in> ->
@@ -17,12 +19,41 @@ class TransducerContext<Recv>(var step: Reducer<Recv,String>) { //initially step
                 else
                     acc }}
 
-    fun main() {
+    inline fun <T> taking(n: Int): Transducer<Recv,T,T> {
+        var count = 0
+        return { step: Reducer<Recv,T> ->
+                { acc: Recv, arg: T ->
+                    count++
+                    if (count > n) {
+                        acc
+                    }
+                    else if (count == n) {
+//                        count++
+                        exit = true
+                        step(acc, arg)
+                    } else {
+//                        count++
+                        step(acc, arg)
+                    }}}
+    }
+
+    inline fun <T_in,T_out> flatMapping(crossinline f: (T_out) -> T_in): Transducer<Recv,T_in,Iterable<T_out>> {
+        return { step: Reducer<Recv,T_in> ->
+                { acc: Recv, arg: Iterable<T_out> ->
+                    for (e in arg) {
+                        if (exit) break
+                        mapping(f)(step)(acc,e)
+                    }
+                    acc
+                }}
+    }
+
+    /*fun main() {
 
         ctx {
             mapping<String,Int> { v -> v.showDoubledString() }(filtering<String> { v -> v.startsWith("4") }(step))
         }
-    }
+    }*/
 
 
 
@@ -30,8 +61,14 @@ class TransducerContext<Recv>(var step: Reducer<Recv,String>) { //initially step
         step = { acc: Recv, arg: Any? -> if (pred((arg as T))) step(acc,arg) else acc }
     }*/
 
-    inline fun transduce() {
+    inline fun <In,Out> transduce(arr: List<In>, start: Out, reducer: Reducer<Out,In>): Out {
+        var acc = start
+        for (e in arr) {
+            if (exit) break
+            acc = reducer(acc, e)
+        }
 
+        return acc
     }
 }
 
--- src/main/kotlin/TransducersKt.kt
+++ src/main/kotlin/TransducersKt.kt
@@ -43,6 +43,7 @@ fun main() {
     val list = listOf(1,2,3)
     val list2 = list.map { it.showDoubledString() }
         .filter { it.startsWith("4") }
+        .take(0)
 
 //    println("Casual: ${ map(list) { it.showDoubledString() } }")
     println("Casual: ${list2}")
@@ -51,12 +52,25 @@ fun main() {
     println("Transduced: ${foldl(list, emptyList(), foo)}")
     println("Ext fun: ${list.mapT { it.showDoubledString() }}")
 
-    val transducerContext = TransducerContext<MutableList<String>> { a, b -> conj(a, b) }
+    val transducerContext = TransducerContext<MutableList<String>,Int,String> { a, b -> conj(a, b) }
     val transducerChain = transducerContext.ctx {
         mapping<String,Int> { v -> v.showDoubledString() }(
-            filtering<String> { v -> v.startsWith("4") }(step))
+            filtering<String> { v -> !v.startsWith("3") }(
+                taking<String>(0)(
+                    step))) //So Clojure, much smiley =)
     }
-    println("Transduced new: ${foldl(list, mutableListOf(), transducerChain)}")
+    println("Transduced new: ${transducerContext.transduce(list, mutableListOf(), transducerChain)}")
+
+    val listList = listOf(listOf(1,2,3), listOf(4,5,6), listOf(7,8,9))
+    val transCtx = TransducerContext<MutableList<Int>,Iterable<Int>,Int> { a, b -> conj(a, b) }
+    val transChain = transCtx.ctx {
+        flatMapping<Int,Int> { el -> el*10 }(
+            taking<Int>(8)(
+                step))
+    }
+    println("Flatmap check: ${transCtx.transduce(listList, mutableListOf(), transChain)}")
+
+   // println("Transduced new: ${foldl(list, mutableListOf(), transducerChain)}")
     /*val pairedList: List<Pair<Int,String>> = listOf(1 to "a", 2 to "b", 3 to "see")
     pairedList.flatMap { listOf(it.first.toString(),it.second) }*/
 
